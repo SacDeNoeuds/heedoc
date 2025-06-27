2269a3f87e283c08f1cfefcb2306d2c535fc138f
SacDeNoeuds <92589074+SacDeNoeuds@users.noreply.github.com>
2025-06-27 19:25:21 +0200

    style(render md reference spec): format code

--- src/render-markdown-reference.spec.ts
+++ src/render-markdown-reference.spec.ts
@@ -1,24 +1,27 @@
-import { describe, expect, it } from 'vitest'
-import type { FileDocumentation } from './parser.js'
-import { markdownReferenceRenderer } from './render-markdown-reference.js'
+import { describe, expect, it } from "vitest"
+import type { FileDocumentation } from "./parser.js"
+import { markdownReferenceRenderer } from "./render-markdown-reference.js"
 
 describe(markdownReferenceRenderer.name, () => {
-  it('renders a fully documented variable', () => {
+  it("renders a fully documented variable", () => {
     const doc: FileDocumentation = {
       array: {
-        description: 'An array of numbers',
-        type: 'number[]',
-        summary: 'It does not do much',
-        remarks: 'Beware of the tiger\nIt can bring its lot of problems',
+        description: "An array of numbers",
+        type: "number[]",
+        summary: "It does not do much",
+        remarks: "Beware of the tiger\nIt can bring its lot of problems",
         examples: [
-          { title: 'Hello World', code: '```ts\nconsole.log("Hello World")\n```' },
-          { code: '```ts\nconsole.log("Oops!")\n```' }
+          {
+            title: "Hello World",
+            code: '```ts\nconsole.log("Hello World")\n```',
+          },
+          { code: '```ts\nconsole.log("Oops!")\n```' },
         ],
       },
     } as const
-    const md = markdownReferenceRenderer({ 'samples/demo.ts': doc })
-    expect(md).toBe(`
-# Reference
+    const md = markdownReferenceRenderer({ "samples/demo.ts": doc })
+    expect(md).toBe(
+      `# Reference
 
 ## \`array\`
 
@@ -39,21 +42,22 @@ console.log("Hello World")
 \`\`\`ts
 console.log("Oops!")
 \`\`\`
-    `.trim())
+    `.trim(),
+    )
   })
-  
-  it('chains and sorts alphabetically 2 exports', () => {
+
+  it("chains and sorts alphabetically 2 exports", () => {
     const doc: FileDocumentation = {
       timeoutInMs: {
-        description: 'Timeout for all XHR requests',
+        description: "Timeout for all XHR requests",
       },
       array: {
-        description: 'An array of numbers',
+        description: "An array of numbers",
       },
     } as const
-    const md = markdownReferenceRenderer({ 'samples/demo.ts': doc })
-    expect(md).toBe(`
-# Reference
+    const md = markdownReferenceRenderer({ "samples/demo.ts": doc })
+    expect(md).toBe(
+      `# Reference
 
 ## \`array\`
 
@@ -62,6 +66,7 @@ An array of numbers
 ## \`timeoutInMs\`
 
 Timeout for all XHR requests
-    `.trim())
+    `.trim(),
+    )
   })
 })
